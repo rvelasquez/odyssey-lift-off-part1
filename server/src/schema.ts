53f7df71038de5a1460948c5bd9b47602f9ce7c3
Romulo Velasquez <rom.velasquez@gmail.com>
2025-03-18 14:51:54 +0000

    Finished basic server implementation from tutorial

---
+++ server/src/schema.ts
@@ -0,0 +1,24 @@
+import gql from "graphql-tag";
+
+const typeDefs = gql`
+# Schema definitions go here
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+}
+"Author of a complete Track or a Module"
+type Author {
+    id: ID!
+    name: String!
+    photo: String
+}
+type Query {
+    tracksForHome: [Track!]!
+}
+`;
+module.exports = typeDefs;
